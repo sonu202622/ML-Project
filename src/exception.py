649b8275e8f1bcaf4e68664e400ef06fede6316b
Sonu Kumar <Sonukumar202622@gmail.com>
2026-01-15 16:41:47 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 #how your message look like and return error message
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
